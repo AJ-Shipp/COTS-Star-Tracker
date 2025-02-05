91681d4116b57f4a4f813f3a78e404e345b44bf9
AJ-Shipp <anthonyjamesshipp@gmail.com>
2025-02-05 09:53:00 -0600

    Created bitmapViewer.py
    File for reading/editing bitmap images.
    
    8: Bitmap file path
    9-10:
    - Image array size
    - Creates a blank array of zeros of the same size
    
    11-13:
    - Opens the file using the PIL.Image.getdata() function
    - Reshapes the image into a numpy array that matches the pixel grid
    
    15: Finds the overall average value for all pixels in the image
    
    17-24:
    - Goes through each pixel and replaces the 'imgSubbed' zeros with the value from the reshaped image.
    - If a pixel is less than 1.2x the average, then it is set to zero to cancel out the noise where possible
    - For each pixel, if it is less than 5% of the pixel ahead of it (x+1,y+1), then the pixel ahead of it is set equal to zero.
    
    28-31:
    - Functionality to plot either the base image or the edited one

---
+++ custom_programs/bitmapViewer.py
@@ -0,0 +1,31 @@
+import os
+import scipy as sci
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL 
+from PIL import Image
+
+file = r"C:\Users\antho\InfraTracker\2-1-25 Trip\image_series_set4-_3.bmp"
+fSize = [int(2592),int(1944)]
+imgSubbed = np.zeros((fSize[1],fSize[0]))
+img = Image.open(file)
+
+imgReshaped = np.reshape(list(img.getdata()),(fSize[1],fSize[0]))
+
+avg = np.average(imgReshaped)
+
+for i in range(fSize[0]):
+    for j in range(fSize[1]):
+        imgSubbed[j][i] = imgReshaped[j][i] #- avg
+        if imgSubbed[j][i] < 1.2*avg:
+            imgSubbed[j][i] = 0
+        if i < fSize[0] or j < fSize[1]:
+            if imgSubbed[j][i] < 0.05*imgSubbed[j-1][i-1]:
+                imgSubbed[j-1][i-1] = 0
+
+print(np.sum(imgSubbed))
+
+# plt1 = plt.imshow(img,'jet')
+plt2 = plt.imshow(imgSubbed,'jet') 
+
+plt.show()
